6302757312771fc65bd7380a74e7e38328c6f3eb
chaabane72 <chaabane72@yahoo.fr>
2025-08-22 16:00:39 +0200

    Mise à jour pxt.json, main.blocks, main.ts, Ma_LED.ts

---
+++ Ma_LED.ts
@@ -0,0 +1,28 @@
+/**
+ * Blocs personnalisés pour contrôler une LED
+ */
+//% color=#ff6600 icon="\uf0eb" weight=70 block="LED"
+namespace MaLED {
+
+    /**
+     * Allumer la LED sur une broche donnée
+     * @param pin la broche où est branchée la LED
+     */
+    //% blockId=allumer_led
+    //% block="allumer LED sur la broche %pin"
+    export function allumer(pin: DigitalPin): void {
+        // On envoie du courant (valeur 1) sur la broche choisie
+        pins.digitalWritePin(pin, 1)
+    }
+
+    /**
+     * Éteindre la LED sur une broche donnée
+     * @param pin la broche où est branchée la LED
+     */
+    //% blockId=eteindre_led
+    //% block="éteindre LED sur la broche %pin"
+    export function eteindre(pin: DigitalPin): void {
+        // On coupe le courant (valeur 0) sur la broche choisie
+        pins.digitalWritePin(pin, 0)
+    }
+}
